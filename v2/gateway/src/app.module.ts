836a45e11d458a59529009442849a40a3c0557b5
ansushina <nastya.sushina@yandex.ru>
2022-01-09 20:48:10 +0300

    23213vxfdffdads

--- v2/gateway/src/app.module.ts
+++ v2/gateway/src/app.module.ts
@@ -11,9 +11,23 @@ import { MessageConsumer } from './services/order/order.consumer';
 @Module({
   imports: [HttpModule, 
     BullModule.forRoot({
+      redis: {
+        host: 'ec2-34-247-243-70.eu-west-1.compute.amazonaws.com',
+        port: 13510,
+        password: 'pb75d8f8fda76b3cd89e321b57db8dda2bea6ae899d705f5db90e4f728aa4d55e',
+        tls: {
+          rejectUnauthorized: false
+      }
+      },
     }),
     BullModule.registerQueue({
       name:'queue1',
+      // redis: {
+      //   // host: 'ec2-34-247-243-70.eu-west-1.compute.amazonaws.com',
+      //   // port: 13510,
+      //   // password: 'pb75d8f8fda76b3cd89e321b57db8dda2bea6ae899d705f5db90e4f728aa4d55e',
+      //   path: 'redis://:pb75d8f8fda76b3cd89e321b57db8dda2bea6ae899d705f5db90e4f728aa4d55e@ec2-34-247-243-70.eu-west-1.compute.amazonaws.com:13510'
+      // }
     })
   ],
   controllers: [AppController],
